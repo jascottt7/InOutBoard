1acf4aff0df9f99f7fc65be43a19ad96f630557e
unknown <J_Scott@JScott.HighlandShoresCAS.local>
2014-04-25 10:19:35 -0400

    second push

--- public/js/todo_list.js
+++ public/js/todo_list.js
@@ -7,10 +7,12 @@ $(document).ready(function() {
       data: { id: staff_id },     
       }).done(function(data) {
         if(data.status == 'Out') {
+          $("#" + data.id + " i").removeClass('icon-ok').addClass('icon-remove')
           $("#" + data.id + " a.status").text('Sign In')
           $("#" + data.id).removeClass('success').addClass('error');
         }
         else {
+          $("#" + data.id + " i").removeClass('icon-remove').addClass('icon-ok')
           $("#" + data.id + " a.status").text('Sign Out')
           $("#" + data.id).removeClass('error').addClass('success');
         }
--- web.rb
+++ web.rb
@@ -2,6 +2,16 @@ require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
 require 'json'
+require 'net-ldap'
+require 'date'
+require 'haml'
+ 
+ldap = Net::LDAP.new
+ldap.host = 'IP Address'
+ldap.port = 389
+ldap.auth "Admin", "Password"
+
+treebase = "ou=************, dc=***********, dc=local"
 
 helpers do
   def protected!
@@ -23,6 +33,11 @@ class Staff
   property :id, Serial
   property :fname, Text, :required => true
   property :lname, Text, :required => true
+  property :email, Text, :required => true
+  property :phone, Text
+  property :location, Text
+  property :department, Text
+  property :title, Text
   property :status, Boolean, :required => true, :default => false
   property :comments, Text
   property :created, DateTime
@@ -31,9 +46,10 @@ DataMapper.finalize.auto_upgrade!
 
 get '/?' do
   protected!
-  @staff = Staff.all(:order => :created.desc)
+  @staff = Staff.all(:order => :fname)
+  @windows_user = ENV['USERNAME']
   redirect '/new' if @staff.empty?
-  erb :index
+  haml :index
 end
 
 get '/new/?' do
@@ -42,7 +58,7 @@ get '/new/?' do
 end
 
 post '/new/?' do
-  Staff.create(:fname => params[:fname], :lname => params[:lname], :created => Time.now)
+  Staff.create(:fname => params[:fname], :lname => params[:lname], :email => params[:email], :created => Time.now)
   redirect '/'
 end
 
@@ -68,4 +84,80 @@ post '/delete/:id/?' do
   else
     redirect '/'
   end
+end  
+
+get '/adusers' do 
+  @staff = Staff.all(:fields => [:id, :email])
+  filter = Net::LDAP::Filter.eq("mail", "*")
+  @b = Hash.new
+  ldap.search(:base => treebase, :filter => filter) do |entry|    
+    if entry.respond_to?("l")
+      email = ""
+      if entry.respond_to?("mail")
+        email = entry.mail
+      end      
+      name = ""
+      if entry.respond_to?("cn")
+        name = entry.cn
+      end     
+      @b[name] = {:name => name, :email => email} 
+    end
+  end
+  haml :add_user_from_ad
 end
+
+  
+get '/email/:id' do 
+  filter = Net::LDAP::Filter.eq("mail", params[:id])
+  @b = Hash.new
+  ldap.search(:base => treebase, :filter => filter) do |entry|    
+    if entry.respond_to?("l")
+  
+      title = ""
+      if entry.respond_to?("title")
+        title = entry.title
+      end
+      
+      logon_name = ""
+      if entry.respond_to?("sAMAccountName")
+        logon_name = entry.sAMAccountName
+      end
+ 
+      email = ""
+      if entry.respond_to?("mail")
+        email = entry.mail
+      end
+    
+      telephonenumber = ""
+      if entry.respond_to?("telephonenumber")
+        telephonenumber = entry.telephonenumber
+      end
+ 
+      location = ""
+      if entry.respond_to?("l")
+        location = entry.l
+      end
+    
+      department = ""
+      if entry.respond_to?("department")
+        department = entry.department
+      end
+      
+      manager = ""
+      if entry.respond_to?("manager")
+        manager = entry.manager
+      end
+      
+      name = ""
+      if entry.respond_to?("cn")
+        name = entry.cn
+      end
+      
+      @b[name] = {:name => name, :email => email, :phone => telephonenumber, :location => location, :department => department, :title => title, :logon_name => logon_name}
+      @manager = {:manager => manager}
+ 
+    end
+  end
+  haml :detail
+end
+  
